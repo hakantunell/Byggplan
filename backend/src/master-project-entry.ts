f7eb8eb92f44566cb6f6ced5eaf57917f2683518
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-07 14:09:05 +0200

    Register master project snapshot routes

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -1,6 +1,8 @@
 import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
+import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
 
 registerMasterProjectRoutes(app as any);
+registerMasterProjectCloneRoutes(app as any);
 
 export default app;
