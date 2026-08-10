eecc329563ed52b996f49426af3bc1440950dfe5
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-10 13:28:49 +0200

    Avoid duplicate project document route registration

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -4,13 +4,11 @@ import { registerMasterProjectCloneRoutes } from './master-project-clone-routes'
 import { registerProjectSupportAttachmentUploadRoutes } from './project-support-attachment-upload-routes';
 import { registerProjectSupportRoutes } from './project-support-routes';
 import { registerProjectSupportJsonUploadRoutes } from './project-support-json-upload-routes';
-import { registerProjectDocumentRoutes } from './project-document-routes';
 
 registerMasterProjectRoutes(app as any);
 registerMasterProjectCloneRoutes(app as any);
 registerProjectSupportAttachmentUploadRoutes(app as any);
 registerProjectSupportRoutes(app as any);
 registerProjectSupportJsonUploadRoutes(app as any);
-registerProjectDocumentRoutes(app as any);
 
 export default app;
