c534ff4d3e58ff3859b7c3b13bea9c642bf21ed1
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-10 10:33:41 +0200

    Register master support resource routes

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -1,8 +1,10 @@
 import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
 import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
+import { registerMasterSupportRoutes } from './master-support-routes';
 
 registerMasterProjectRoutes(app as any);
 registerMasterProjectCloneRoutes(app as any);
+registerMasterSupportRoutes(app as any);
 
 export default app;
