2408c6237886d7af2e5e22ae6106dfb95f5ebfee
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-10 13:12:50 +0200

    Register collision-free attachment upload route

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -1,11 +1,13 @@
 import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
 import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
+import { registerProjectSupportAttachmentUploadRoutes } from './project-support-attachment-upload-routes';
 import { registerProjectSupportRoutes } from './project-support-routes';
 import { registerProjectSupportJsonUploadRoutes } from './project-support-json-upload-routes';
 
 registerMasterProjectRoutes(app as any);
 registerMasterProjectCloneRoutes(app as any);
+registerProjectSupportAttachmentUploadRoutes(app as any);
 registerProjectSupportRoutes(app as any);
 registerProjectSupportJsonUploadRoutes(app as any);
 
