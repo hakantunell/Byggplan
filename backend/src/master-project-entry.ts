ae6f366bd9554162a39eb438b9b6cdfcf81432c5
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-10 12:31:15 +0200

    Register JSON project support upload route

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -2,9 +2,11 @@ import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
 import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
 import { registerProjectSupportRoutes } from './project-support-routes';
+import { registerProjectSupportJsonUploadRoutes } from './project-support-json-upload-routes';
 
 registerMasterProjectRoutes(app as any);
 registerMasterProjectCloneRoutes(app as any);
 registerProjectSupportRoutes(app as any);
+registerProjectSupportJsonUploadRoutes(app as any);
 
 export default app;
