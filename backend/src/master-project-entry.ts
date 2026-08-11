3059af27325698ff953718e8b7dad866f9d4688a
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-11 13:40:20 +0200

    Register master project module routes

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -1,6 +1,7 @@
 import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
 import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
+import { registerMasterProjectModuleRoutes } from './master-project-module-routes';
 import { registerProjectSupportAttachmentUploadRoutes } from './project-support-attachment-upload-routes';
 import { registerProjectSupportRoutes } from './project-support-routes';
 import { registerProjectSupportJsonUploadRoutes } from './project-support-json-upload-routes';
@@ -10,6 +11,7 @@ import { registerProjectManagementRoutes } from './project-management-routes';
 import { registerGoverningDocumentFileRoutes } from './governing-document-file-routes';
 
 registerMasterProjectRoutes(app as any);
+registerMasterProjectModuleRoutes(app as any);
 registerMasterProjectCloneRoutes(app as any);
 registerProjectSupportAttachmentUploadRoutes(app as any);
 registerProjectSupportRoutes(app as any);
