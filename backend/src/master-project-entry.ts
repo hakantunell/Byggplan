96b60b9a70f1f4990db6fb54af17cd212ab46633
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-12 15:20:09 +0200

    Advance Master reconciliation to v14

--- backend/src/master-project-entry.ts
+++ backend/src/master-project-entry.ts
@@ -2,7 +2,7 @@ import app from './studio-import-entry';
 import { registerMasterProjectRoutes } from './master-project-routes';
 import { registerMasterProjectCloneRoutes } from './master-project-clone-routes';
 import { registerMasterProjectModuleRoutes } from './master-project-module-routes';
-import { ensureMasterV13, registerMasterProjectV2UpgradeRoutesV13 } from './master-project-v2-upgrade-routes-v13';
+import { ensureMasterV14, registerMasterProjectV2UpgradeRoutesV14 } from './master-project-v2-upgrade-routes-v14';
 import { registerProjectSupportAttachmentUploadRoutes } from './project-support-attachment-upload-routes';
 import { registerProjectSupportRoutes } from './project-support-routes';
 import { registerProjectSupportJsonUploadRoutes } from './project-support-json-upload-routes';
@@ -13,12 +13,12 @@ import { registerProjectMasterDiagnosticsRoutes } from './project-master-diagnos
 import { registerProjectMasterRepairRoutes } from './project-master-repair-routes';
 import { registerGoverningDocumentFileRoutes } from './governing-document-file-routes';
 
-const MASTER_V2_TARGET_VERSION=13;
+const MASTER_V2_TARGET_VERSION=14;
 
 async function reconcileMaster(c:any,next:any){
   if(c.req.method==='GET'){
     const master=await c.env.DB.prepare("SELECT id,version FROM master_projects WHERE code='fritidshus-v2'").first<any>();
-    if(master&&Number(master.version||0)<MASTER_V2_TARGET_VERSION)await ensureMasterV13(c.env.DB,String(master.id));
+    if(master&&Number(master.version||0)<MASTER_V2_TARGET_VERSION)await ensureMasterV14(c.env.DB,String(master.id));
   }
   await next();
 }
@@ -27,7 +27,7 @@ app.use('/api/studio/master-projects/*',reconcileMaster);
 
 registerMasterProjectRoutes(app as any);
 registerMasterProjectModuleRoutes(app as any);
-registerMasterProjectV2UpgradeRoutesV13(app as any);
+registerMasterProjectV2UpgradeRoutesV14(app as any);
 registerMasterProjectCloneRoutes(app as any);
 registerProjectSupportAttachmentUploadRoutes(app as any);
 registerProjectSupportRoutes(app as any);
