221e0af5e63cced6eda735ab3ac35fdd26e893ea
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-13 08:21:56 +0200

    Use reviewed project activity creation routes

--- backend/src/governing-mapping-routes-v3.ts
+++ backend/src/governing-mapping-routes-v3.ts
@@ -1,1 +1,1 @@
-export { registerGoverningMappingRoutesV16 as registerGoverningMappingRoutesV3 } from './governing-mapping-routes-v16';
+export { registerGoverningMappingRoutesV17 as registerGoverningMappingRoutesV3 } from './governing-mapping-routes-v17';
