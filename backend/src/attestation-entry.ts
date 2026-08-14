aca042f408a8ae759ba0c7cdcc9a5b70e2bfa0ed
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-14 21:40:00 +0200

    Register project document annotation routes

--- backend/src/attestation-entry.ts
+++ backend/src/attestation-entry.ts
@@ -2,9 +2,11 @@ import app from './master-project-entry';
 import { registerGoverningAttestationRoutes } from './governing-attestation-routes';
 import { registerActivityOwnDocumentationRoutes } from './activity-own-documentation-routes';
 import { registerProjectDocumentCategoryRoutes } from './project-document-category-routes';
+import { registerProjectDocumentAnnotationRoutes } from './project-document-annotation-routes';
 
 registerGoverningAttestationRoutes(app as any);
 registerActivityOwnDocumentationRoutes(app as any);
 registerProjectDocumentCategoryRoutes(app as any);
+registerProjectDocumentAnnotationRoutes(app as any);
 
 export default app;
