145c247e65facd56249734140502b99682dce968
hakantunell <49018375+hakantunell@users.noreply.github.com>
2026-08-14 10:59:49 +0200

    Register activity own-documentation routes

--- backend/src/attestation-entry.ts
+++ backend/src/attestation-entry.ts
@@ -1,6 +1,8 @@
 import app from './master-project-entry';
 import { registerGoverningAttestationRoutes } from './governing-attestation-routes';
+import { registerActivityOwnDocumentationRoutes } from './activity-own-documentation-routes';
 
 registerGoverningAttestationRoutes(app as any);
+registerActivityOwnDocumentationRoutes(app as any);
 
 export default app;
